66acb95ec0c9ac0c72dda75cdf3bff887b2f0313
sangjinsu <climbplant39@kakao.com>
2023-05-30 23:08:57 +0900

    r graphics

---
+++ r-graphics.R
@@ -0,0 +1,16 @@
+# 단변수 범주형 자료 
+
+library(MASS)
+head(Cars93)
+
+# barplot
+# pie 
+
+# 연속형 단변수 자료 
+# hist
+
+library(vcd)
+summary(Arthritis)
+head(Arthritis)
+with(Arthritis, plot(density(Age)))
+
